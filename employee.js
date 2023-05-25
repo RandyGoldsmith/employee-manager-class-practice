095f3ce533dcef53ff84c41827aed1895ac96306
Randy Goldsmith <randyg@Randys-MacBook-Pro.local>
2023-05-24 23:41:36 -0500

    finished phase3

--- employee.js
+++ employee.js
@@ -1,14 +1,19 @@
 class Employee {
-    constructor(name, salary, title, manager) {
+    constructor(name, salary, title, manager = null) {
         this.name = name;
         this.salary = salary;
         this.title = title;
-        if(!manager) {
-            this.manager = null;
-        } else {
-            this.manager = manager;
+        this.manager = manager;
+
+        if(manager !== null) {
+            manager.addEmployee(this);
         }
     }
+    calculateBonus(multiplier) {
+        return this.salary * multiplier;
+    }
 }
+
+x
 
 module.exports = Employee;
