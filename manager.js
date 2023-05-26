f91b4f7754609bc0d50d3a3c69e48258738591fa
Randy Goldsmith <randyg@Randys-MacBook-Pro.local>
2023-05-26 11:25:04 -0500

    finished phase 4

--- manager.js
+++ manager.js
@@ -12,31 +12,25 @@ class Manager extends Employee {
             this.employees.push(employee);
     }
 
-    _totalSubSalary(employee) {
-        let sum = 0;
-        for(let i = 0; i < this.employees.length; i++) {
-            if(employee.employees[i] instanceof Manager) {
-                sum = this.employee + this._totalSubSalary();
-            } else {
-                sum = this.employee.salary;
-            }
-        }
-        
-    }
-
     calculateBonus(multiplier) {
         return (this.salary + this._totalSubSalary()) * multiplier;
     }
+
+    _totalSubSalary() {
+        let sum = 0;
+        for(let employee of this.employees) {
+            if(employee instanceof Manager) {
+                sum+= employee.salary + employee._totalSubSalary();
+            } else {
+                sum+= employee.salary;
+            }
+        }
+        return sum;
+    }
+
+    
 }
 
-const splinter = new Manager('Splinter', 100000, 'Sensei');
-const leo = new Manager('Leonardo', 90000, 'Ninja', splinter);
-const raph = new Manager('Raphael', 90000, 'Ninja', leo);
-const mikey = new Employee('Michelangelo', 85000, 'Grasshopper', raph);
-const donnie = new Employee('Donatello', 85000, 'Grasshopper', raph);
-
-console.log(splinter.calculateBonus(0.05)); // => 22500
-console.log(leo.calculateBonus(0.05)); // => 17500
-console.log(raph.calculateBonus(0.05)); // => 13000
+c
 
 module.exports = Manager;
